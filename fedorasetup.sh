b896b09b33f07fa93985dfadfdc41def8ca68676
nsmhere <58481039+itsnsmyk@users.noreply.github.com>
2023-05-14 01:53:19 +0530

    Update fedorasetup.sh

--- fedorasetup.sh
+++ fedorasetup.sh
@@ -100,8 +100,8 @@ sudo dnf install -y okular mpv;
 #auto-cpufreq
 echo 'Enabling auto-cpufreq to save your battery'
 git clone https://github.com/AdnanHodzic/auto-cpufreq.git
-cd auto-cpufreq && sudo ./auto-cpufreq-installer;
-sudo auto-cpufreq --install;
+cd auto-cpufreq/ && sudo ./auto-cpufreq-installer
+sudo auto-cpufreq --install
 
 #Gaming
 #Steam
