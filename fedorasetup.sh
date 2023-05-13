120999632efa9eb7de42d8bc629121254419ca3c
nsmhere <58481039+itsnsmyk@users.noreply.github.com>
2023-05-13 11:07:41 +0530

    Update fedorasetup.sh

--- fedorasetup.sh
+++ fedorasetup.sh
@@ -57,10 +57,10 @@ sudo rpmkeys --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw
 printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=download.vscodium.com\nbaseurl=https://download.vscodium.com/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg\nmetadata_expire=1h" | sudo tee -a /etc/yum.repos.d/vscodium.repo;
 sudo dnf install -y codium;
 
-#WPS
+#OnlyOffice
 
-axel -n 10 https://wdl1.pcfg.cache.wpscdn.com/wpsdl/wpsoffice/download/linux/11698/wps-office-11.1.0.11698.XA-1.x86_64.rpm;
-sudo rpm -i wps-office-11.1.0.11698.XA-1.x86_64.rpm;
+axel -n 10 https://github.com/ONLYOFFICE/appimage-desktopeditors/releases/download/v7.3.3/DesktopEditors-x86_64.AppImage;
+
 
 #Email_Client
 axel -n 10 https://updates.getmailspring.com/download?platform=linuxRpm;
@@ -85,21 +85,11 @@ sudo dnf install -y qemu-kvm libvirt virt-install bridge-utils virt-manager libv
 #ncspot(terminal spotify)
 
 echo 'Installing ncspot'
-sudo dnf install -y dbus-devel libxcb-devel ncurses-devel openssl-devel pulseaudio-libs-devel;
-sudo dnf install -y cargo;
-cargo install ncspot;
+flatpak install flathub io.github.hrkfdn.ncspot;
 
 #small_apps
 
-sudo dnf install -y okular mpv flameshot 
-
-#autocpufreq 
-
-echo 'Installing auto-cpufreq'
-
-git clone https://github.com/AdnanHodzic/auto-cpufreq.git
-cd auto-cpufreq && sudo ./auto-cpufreq-installer && cd .. ;
-
+sudo dnf install -y okular mpv
 
 sync
 
