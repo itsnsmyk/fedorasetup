cb3f1feb7ace6e987a55dc9b16cc25e0a867ec90
nsmhere <58481039+itsnsmyk@users.noreply.github.com>
2023-05-18 02:10:51 +0530

    Create uninstallgaming.sh

---
+++ uninstallgaming.sh
@@ -0,0 +1,12 @@
+#Steam
+
+flatpak uninstall flathub com.valvesoftware.Steam
+
+#Heroic
+
+flatpak uninstall flathub com.heroicgameslauncher.hgl
+
+#Protonup-qt
+flatpak uninstall flathub net.davidotek.pupgui2
+
+sudo dnf clean all
